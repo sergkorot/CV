02540f19fd23b735723f15b5a5162ba45907d47d
Sergey Korotkov <sergkorotkov5@gmail.com>
2022-02-28 16:37:07 +0300

    init:start

---
+++ index.js
@@ -0,0 +1,13 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav-list');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+ 
+    
+
+
+
+
+	
